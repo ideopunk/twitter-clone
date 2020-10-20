63364d835bae87228e5e9d94e4800d5ef2ffd7ab
Conor Barnes <conorbarnes93@gmail.com>
2020-10-20 13:16:54 -0700

    Tweet styling

--- src/components/reusables/Feed.js
+++ src/components/reusables/Feed.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import Tweet from "./Tweet";
+import headshot from "../../assets/headshot.png"
 const Feed = () => {
-	return <div>feed</div>;
+	return (
+		<div>
+			<Tweet
+				image={headshot}
+				name="Conor"
+				at={"@ideopunk"}
+				time="March 4"
+				text="This is a tweet! Woo!"
+				retweets={4}
+				likes={4}
+			/>
+		</div>
+	);
 };
 
 export default Feed;
 
-// used in Home, Explore, Profile, and modified in Notifications. 
+// used in Home, Explore, Profile, and modified in Notifications.
