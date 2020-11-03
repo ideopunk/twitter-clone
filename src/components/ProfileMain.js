85c23cf0c671ece38ef0f126ff738a81c8aa702b
Conor Barnes <conorbarnes93@gmail.com>
2020-11-03 14:14:12 -0800

    profilemain Works

--- src/components/ProfileMain.js
+++ src/components/ProfileMain.js
@@ -22,57 +22,61 @@ const ProfileMain = (props) => {
 
 	const { userProfile, profileID } = props;
 	console.log(userProfile, profileID);
-	const [profileData, setProfileData] = useState({});
+	const [profileData, setProfileData] = useState({follows: [], followers: []});
 
 	const [tweetDatas, setTweetDatas] = useState([]);
 
 	const [editor, setEditor] = useState(false);
 
-	const [header, setHeader] = useState(null);
-	const [propic, setPropic] = useState(null);
-	const [bio, setBio] = useState("");
-	const [website, setWebsite] = useState("");
-	const [joinDate, setJoinDate] = useState(null);
-	const [at, setAt] = useState("");
-	const [name, setName] = useState("");
-	const [follows, setFollows] = useState("");
-	const [followers, setFollowers] = useState("");
 	const [followed, setFollowed] = useState("");
+
+	// consoling
+	useEffect(() => {
+		console.log("profiledata");
+		console.log(profileData);
+	}, [profileData]);
 
 	// set user data
 	useEffect(() => {
 		console.log("set user data");
 
-		const itsCurrent = () => {
-			// setAt(userAt);
-			// setName(userName);
-			// setFollows(userFollows);
-			// setFollowers(userFollowers);
-
-			setProfileData({
-				at: userAt,
-				name: userName,
-				follows: userFollows,
-				followers: userFollowers,
-				id: userID,
-				image: userImage,
-			});
-		};
-
 		userProfile
-			? itsCurrent()
+			? setProfileData({
+					at: userAt,
+					name: userName,
+					follows: userFollows || [],
+					followers: userFollowers || [],
+					id: userID,
+					image: userImage,
+			  })
 			: db
 					.collection("users")
-					.where("at", "==", urlAt)
+					.doc(profileID)
 					.get()
-					.then((snapshot) => {
-						snapshot.forEach((doc) => {
-							console.log(doc);
+					.then((doc) => {
+						const data = doc.data();
 
-							setProfileData({ id: doc.id, ...doc.data() });
-						});
+						// if they've got bio and website, add em in.
+						// data.bio && setBio(data.bio);
+						// data.website && setWebsite(data.website);
+						// data.joinDate && setJoinDate(new Date(data.joinDate.seconds * 1000));
+						// setFollowers(data.followers);
+						// setFollows(data.follows);
+						// setAt(data.at);
+						// setName(data.name);
+						setProfileData((prevData) => ({
+							...prevData,
+							at: data.at,
+							name: data.name,
+							follows: data.follows || [],
+							followers: data.followers || [],
+							bio: data.bio,
+							id: doc.id,
+							website: data.website,
+							joinDate: new Date(data.joinDate.seconds * 1000),
+						}));
 					});
-	}, [userProfile, userImage, userAt, userID, userName, userFollowers, userFollows, urlAt]);
+	}, [userProfile, profileID, userImage, userAt, userID, userName, userFollowers, userFollows]);
 
 	// set header
 	useEffect(() => {
@@ -98,17 +102,17 @@ const ProfileMain = (props) => {
 		console.log("set pro pic");
 
 		userProfile
-			? //  setPropic(userImage)
-			  setProfileData((prevData) => ({ ...prevData, image: userImage }))
+			? setProfileData((prevData) => ({ ...prevData, image: userImage }))
 			: storage
 					.ref("profile_pictures/" + profileID + ".png")
 					.getDownloadURL()
 					.then((url) => {
-						setPropic(url);
+						// setPropic(url);
+						setProfileData((prevData) => ({ ...prevData, image: url }));
 					})
 					.catch((e) => {
 						console.log(e);
-						setPropic(Leaf);
+						// setPropic(Leaf);
 						setProfileData((prevData) => ({ ...prevData, image: Leaf }));
 					});
 	}, [userProfile, userImage, profileID]);
@@ -133,41 +137,6 @@ const ProfileMain = (props) => {
 			});
 	}, [profileID]);
 
-	// set data if not own profile....
-	useEffect(() => {
-		console.log("set data if not own profile");
-
-		!userProfile &&
-			db
-				.collection("users")
-				.doc(profileID)
-				.get()
-				.then((doc) => {
-					const data = doc.data();
-
-					// if they've got bio and website, add em in.
-					data.bio && setBio(data.bio);
-					data.website && setWebsite(data.website);
-					data.joinDate && setJoinDate(new Date(data.joinDate.seconds * 1000));
-					if (!userProfile) {
-						setFollowers(data.followers);
-						setFollows(data.follows);
-						setAt(data.at);
-						setName(data.name);
-						setProfileData((prevData) => ({
-							...prevData,
-							at: data.at,
-							name: data.name,
-							follows: data.follows,
-							followers: data.followers,
-							bio: data.bio,
-							website: data.website,
-							joinDate: new Date(data.joinDate.seconds * 1000),
-						}));
-					}
-				});
-	}, [profileID, userImage, userProfile, userFollows]);
-
 	// if this isn't our own profile, are we following this user?
 	useEffect(() => {
 		console.log("set following");
@@ -190,13 +159,13 @@ const ProfileMain = (props) => {
 					>
 						<SideArrow />
 						<div className="profile-home-link-text">
-							<h3 className="no-dec">{name}</h3>
+							<h3 className="no-dec">{profileData.name}</h3>
 							<p className="grey">{tweetDatas.length} tweets</p>
 						</div>
 					</Link>
-					<img className="profile-header-image" src={header} alt="header" />
+					<img className="profile-header-image" src={profileData.header} alt="header" />
 					<div className="profile-card">
-						<img className="main-image" src={propic} alt="profile" />
+						<img className="main-image" src={profileData.image} alt="profile" />
 						{userProfile ? (
 							<div style={{ height: "3rem" }}>
 								<button
@@ -210,26 +179,29 @@ const ProfileMain = (props) => {
 						) : (
 							<FollowButton tweeterID={profileID} followed={followed} />
 						)}
-						<h3>{name}</h3>
-						<p className="grey">{at}</p>
-						<p className="bio">{bio}</p>
+						<h3>{profileData.name}</h3>
+						<p className="grey">{profileData.at}</p>
+						<p className="bio">{profileData.bio}</p>
 						<p className="grey">
-							<span>{website}</span>
+							<span>{profileData.website}</span>
 							<span>
 								{" "}
 								Joined{" "}
-								{String(joinDate).slice(4, 8) + String(joinDate).slice(11, 16)}
+								{String(profileData.joinDate).slice(4, 8) +
+									String(profileData.joinDate).slice(11, 16)}
 							</span>
 						</p>
 						<p>
 							<Link to={`${url}/following`}>
 								<span style={{ marginRight: "1rem" }}>
-									{follows.length} <span className="grey">Following</span>
+									{profileData.follows.length}{" "}
+									<span className="grey">Following</span>
 								</span>
 							</Link>
 							<Link to={`${url}/followers`}>
 								<span>
-									{followers.length} <span className="grey">Followers</span>
+									{profileData.followers.length}{" "}
+									<span className="grey">Followers</span>
 								</span>
 							</Link>
 						</p>
@@ -289,7 +261,11 @@ const ProfileMain = (props) => {
 
 			{editor ? (
 				<Cover toggle={toggleEditor}>
-					<Editor header={header} bio={bio} website={website} />
+					<Editor
+						header={profileData.header}
+						bio={profileData.bio}
+						website={profileData.website}
+					/>
 				</Cover>
 			) : (
 				""
